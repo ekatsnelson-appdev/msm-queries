6331bac76f79b67fd4b34692394077cd3832b51d
ekatsnelson <elisek222@gmail.com>
2022-10-26 19:51:20 +0000

    1:18

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,4 +13,15 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/eldest.html.erb" })
   end
 
+  def director_details
+    @oldest = Director.order({ :dob => :asc }).where.not({ :dob => nil }).at(0)
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).at(0)
+
+    render({ :template => "director_templates/show.html.erb" })
+  end
+
+
 end
